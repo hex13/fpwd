f1c712f927f9b0e9be49eae96197e999640c602d
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-05-17 16:43:33 +0200

    question repository

--- repositories/question.js
+++ repositories/question.js
@@ -1,18 +1,45 @@
-const { readFile } = require('fs/promises')
+const { readFile, writeFile } = require('fs/promises')
 
 const makeQuestionRepository = fileName => {
+  let questions
+  const load = async () => {
+    questions = getQuestions()
+  }
+  const save = async (questions) => {
+    await writeFile(fileName, JSON.stringify(questions), { encoding: 'utf-8' })
+  };
+
   const getQuestions = async () => {
     const fileContent = await readFile(fileName, { encoding: 'utf-8' })
     const questions = JSON.parse(fileContent)
-
     return questions
   }
 
-  const getQuestionById = async questionId => {}
-  const addQuestion = async question => {}
-  const getAnswers = async questionId => {}
-  const getAnswer = async (questionId, answerId) => {}
-  const addAnswer = async (questionId, answer) => {}
+  const getQuestionById = async (questionId, questions = null) => {
+    if (!questions) questions = await getQuestions()
+    return questions.find(question => question.id == questionId);
+  }
+  const addQuestion = async question => {
+    const questions = await getQuestions();
+    questions.push(question);
+    await save(questions);
+  }
+
+  const getAnswers = async questionId => {
+    const question = await getQuestionById(questionId);
+    return question?.answers;
+  }
+  const getAnswer = async (questionId, answerId) => {
+    const answers = await getAnswers(questionId);
+    return answers?.find(answer => answer.id == answerId)
+  }
+
+  const addAnswer = async (questionId, answer) => {
+    const questions = await getQuestions()
+    const question = await getQuestionById(questionId, questions)
+    question.answers.push(answer)
+    await save(questions)
+  }
 
   return {
     getQuestions,
--- repositories/question.test.js
+++ repositories/question.test.js
@@ -2,18 +2,50 @@ const { writeFile, rm } = require('fs/promises')
 const { faker } = require('@faker-js/faker')
 const { makeQuestionRepository } = require('./question')
 
+const createTestQuestions = () => [
+  {
+    "id": "50f9e662-fa0e-4ec7-b53b-7845e8f821c3",
+    "author": "John Stockton",
+    "summary": "What is the shape of the Earth?",
+    "answers": [
+      {
+        "id": "ce7bddfb-0544-4b14-92d8-188b03c41ee4",
+        "author": "Brian McKenzie",
+        "summary": "The Earth is flat."
+      },
+      {
+        "id": "d498c0a3-5be2-4354-a3bc-78673aca0f31",
+        "author": "Dr Strange",
+        "summary": "It is egg-shaped."
+      }
+    ]
+  },
+  {
+    "id": "00f3dd43-ae53-4430-8da1-b722e034c73d",
+    "author": "Sarah Nickle",
+    "summary": "Who let the dogs out?",
+    "answers": []
+  }
+];
+
 describe('question repository', () => {
-  const TEST_QUESTIONS_FILE_PATH = 'test-questions.json'
+  let testQuestionsFilePath
   let questionRepo
+  let testQuestions
 
-  beforeAll(async () => {
-    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify([]))
+  const writeQuestions = async (questions) => {
+    await writeFile(testQuestionsFilePath, JSON.stringify(questions))
+  }
 
-    questionRepo = makeQuestionRepository(TEST_QUESTIONS_FILE_PATH)
+  beforeEach(async () => {
+    testQuestionsFilePath = 'mock-questions-' + faker.datatype.uuid()
+    testQuestions = createTestQuestions()
+    questionRepo = makeQuestionRepository(testQuestionsFilePath)
+    await writeQuestions([])
   })
 
-  afterAll(async () => {
-    await rm(TEST_QUESTIONS_FILE_PATH)
+  afterEach(async () => {
+    await rm(testQuestionsFilePath)
   })
 
   test('should return a list of 0 questions', async () => {
@@ -36,8 +68,118 @@ describe('question repository', () => {
       }
     ]
 
-    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+    await writeQuestions(testQuestions);
 
     expect(await questionRepo.getQuestions()).toHaveLength(2)
   })
+
+  test('should return question by id', async () => {
+    const expectedQuestion = {
+      id: faker.datatype.uuid(),
+      summary: 'What is my name?',
+      author: 'Jack London',
+      answers: []
+    };
+
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Robert De Niro',
+        answers: []
+      },
+      expectedQuestion,
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+
+    await writeQuestions(testQuestions);
+    const question = await questionRepo.getQuestionById(expectedQuestion.id);
+    expect(question).toStrictEqual(expectedQuestion);
+  })
+
+  test('should return undefined where there\'s no question', async () => {
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Robert De Niro',
+        answers: []
+      },
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+
+    await writeQuestions(testQuestions)
+    const question = await questionRepo.getQuestionById('non existing')
+    expect(question).toBe(undefined);
+  })
+
+  test('getAnswers() should return answers', async () => {
+    await writeQuestions(createTestQuestions());
+    const id = "50f9e662-fa0e-4ec7-b53b-7845e8f821c3";
+    const answers = await questionRepo.getAnswers(id)
+    expect(answers).toStrictEqual(createTestQuestions()[0].answers);
+  })
+
+  test('getAnswers() should return undefined', async () => {
+    await writeQuestions(createTestQuestions());
+    const id = "50f9e662-fa0e-4ec7-b53b-7845e8f821c3";
+    const answers = await questionRepo.getAnswers('foo')
+    expect(answers).toBe(undefined);
+  })
+
+  test('getAnswer() should return answer', async () => {
+    await writeQuestions(createTestQuestions());
+    const questionId = "50f9e662-fa0e-4ec7-b53b-7845e8f821c3";
+    const answer = await questionRepo.getAnswer(questionId, "d498c0a3-5be2-4354-a3bc-78673aca0f31")
+    expect(answer).toStrictEqual({
+      "id": "d498c0a3-5be2-4354-a3bc-78673aca0f31",
+      "author": "Dr Strange",
+      "summary": "It is egg-shaped."
+    })
+  })
+  
+  test('getAnswer() should return undefined', async () => {
+    await writeQuestions(createTestQuestions());
+    expect(await questionRepo.getAnswer("bar", "00f3dd43-ae53-4430-8da1-b722e034c73d")).toBe(undefined);
+  })
+
+  test('addQuestion() should add question, addAnswer() should add answer', async () => {
+    const questionId = '6c561c0c-a289-4336-950f-476c5fc36e12';
+    const answerId = '7b8c408e-2a59-447c-a3ca-b218c54e6a92';
+    const createTestQuestion = () => ({
+      id: questionId,
+      author: 'Elon Musk',
+      summary: 'When will you fly to Mars?',
+      answers: [],
+    })
+    const createTestAnswer = () => ({
+      id: answerId,
+      author: 'Elon Musk',
+      summary: 'Tomorrow',
+    })
+
+    await writeQuestions(createTestQuestions())
+    await questionRepo.addQuestion(createTestQuestion())
+    expect(await questionRepo.getQuestionById(questionId)).toStrictEqual(createTestQuestion())
+    
+    await questionRepo.addAnswer(questionId, createTestAnswer())
+
+    expect(await questionRepo.getAnswer(questionId, answerId)).toStrictEqual(createTestAnswer())
+    expect(await questionRepo.getQuestionById(questionId)).toStrictEqual({
+      ...createTestQuestion(),
+      answers: [createTestAnswer()]
+    })
+
+  })
+
 })
