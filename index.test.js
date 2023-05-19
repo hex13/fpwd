c89f12af76a9db9e836f20f98f6af8dcd149aef9
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-05-19 13:52:53 +0200

    💄 prettier: index.test.js

--- index.test.js
+++ index.test.js
@@ -1,91 +1,86 @@
-const request = require('supertest');
-const { makeQuestionRepository } = require('./repositories/question');
-const { createApp: createExpressApp } = require('./index');
+const request = require('supertest')
+const { makeQuestionRepository } = require('./repositories/question')
+const { createApp: createExpressApp } = require('./index')
 const { faker } = require('@faker-js/faker')
 
 function createAppAndRepos() {
+  const questionRepo = {
+    getAnswer: (questionId, answerId) => {
+      return ['getAnswer', questionId, answerId]
+    },
+    getAnswers: id => {
+      return ['getAnswers', id]
+    },
+    addQuestion: jest.fn(() => {}),
+    getQuestions: () => ['getQuestions'],
+    getQuestionById: async id => ['getQuestionById', id]
+  }
 
-   const questionRepo = {
-      getAnswer: (questionId, answerId) => {
-         return (['getAnswer', questionId, answerId]);
-      },
-      getAnswers: (id) => {
-         return (['getAnswers', id])
-      },
-      addQuestion: jest.fn(() => {}),
-      getQuestions: () => (['getQuestions']),
-      getQuestionById: async (id) => (['getQuestionById', id]),
-   }
+  const makeRepositories = () => (req, res, next) => {
+    req.repositories = {
+      questionRepo
+    }
+    next()
+  }
 
-   const makeRepositories = () => (req, res, next) => {
-      req.repositories = {
-         questionRepo,
-      }
-      next()
-   } 
-
-   return { app: createExpressApp(makeRepositories), questionRepo };
+  return { app: createExpressApp(makeRepositories), questionRepo }
 }
 
-
 describe('index', () => {
-   let app, questionRepo
-   beforeEach(() => {
-      ({ app, questionRepo } = createAppAndRepos());
-   })
+  let app, questionRepo
+  beforeEach(() => {
+    ;({ app, questionRepo } = createAppAndRepos())
+  })
 
-   it('GET /', async () => {
-      const response = await request(app)
-         .get('/')
-         .expect('Content-Type', /json/);
-   });
-   it('GET /questions', async () => {
-      const response = await request(app)
-         .get('/questions')
-         .expect('Content-Type', /json/);
-      
-      expect(response.body).toEqual(['getQuestions'])
-   });
-   it('POST /questions should add question', async () => {
-      const response = await request(app)
-         .post('/questions')
-         .send({abc: 'foo'})
-         .expect('Content-Type', /json/);
-      
-      expect(questionRepo.addQuestion.mock.calls).toEqual([[{abc: 'foo'}]])
+  it('GET /', async () => {
+    const response = await request(app).get('/').expect('Content-Type', /json/)
+  })
+  it('GET /questions', async () => {
+    const response = await request(app)
+      .get('/questions')
+      .expect('Content-Type', /json/)
 
-   });
+    expect(response.body).toEqual(['getQuestions'])
+  })
+  it('POST /questions should add question', async () => {
+    const response = await request(app)
+      .post('/questions')
+      .send({ abc: 'foo' })
+      .expect('Content-Type', /json/)
 
-   it('GET /questions/:questionId', async () => {
-      const id = faker.datatype.uuid();
-      const response = await request(app)
-         .get(`/questions/${id}`)
-         .expect('Content-Type', /json/);
-      
-      const question = response.body;
-      expect(question).toEqual(['getQuestionById', id])
-   });
+    expect(questionRepo.addQuestion.mock.calls).toEqual([[{ abc: 'foo' }]])
+  })
 
-   it('GET /questions/:questionId/answers', async () => {
-      const questionId = faker.datatype.uuid();
-      const response = await request(app)
-         .get(`/questions/${questionId}/answers`)
-         .expect('Content-Type', /json/);
+  it('GET /questions/:questionId', async () => {
+    const id = faker.datatype.uuid()
+    const response = await request(app)
+      .get(`/questions/${id}`)
+      .expect('Content-Type', /json/)
 
-      const question = response.body;
+    const question = response.body
+    expect(question).toEqual(['getQuestionById', id])
+  })
 
-      expect(question).toEqual(['getAnswers', questionId])
-   });
+  it('GET /questions/:questionId/answers', async () => {
+    const questionId = faker.datatype.uuid()
+    const response = await request(app)
+      .get(`/questions/${questionId}/answers`)
+      .expect('Content-Type', /json/)
 
-   it('GET /questions/:questionId/answers/:answerId', async () => {
-      const questionId = faker.datatype.uuid();
-      const answerId = faker.datatype.uuid();
-      const response = await request(app)
-         .get(`/questions/${questionId}/answers/${answerId}`)
-         .expect('Content-Type', /json/);
+    const question = response.body
 
-      const question = response.body;
+    expect(question).toEqual(['getAnswers', questionId])
+  })
 
-      expect(question).toEqual(['getAnswer', questionId, answerId])
-   });
-});
+  it('GET /questions/:questionId/answers/:answerId', async () => {
+    const questionId = faker.datatype.uuid()
+    const answerId = faker.datatype.uuid()
+    const response = await request(app)
+      .get(`/questions/${questionId}/answers/${answerId}`)
+      .expect('Content-Type', /json/)
+
+    const question = response.body
+
+    expect(question).toEqual(['getAnswer', questionId, answerId])
+  })
+})
